b1c180a569e72049adf065b559f67639cf1c51e5
in-fin-i-ty <128904385+in-fin-i-ty@users.noreply.github.com>
2023-05-20 16:27:24 +0300

    add block try\except

--- src/video.py
+++ src/video.py
@@ -12,16 +12,23 @@ class Video:
     - количество лайков
     """
     def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        api_key: str = os.getenv('API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=video_id).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count = video_response['items'][0]['statistics']['commentCount']
-        self.url = f'https://www.youtube.com/video/{self.video_id}'
+        try:
+            self.video_id = video_id
+            api_key: str = os.getenv('API_KEY')
+            self.youtube = build('youtube', 'v3', developerKey=api_key)
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=video_id).execute()
+            self.video_title = video_response['items'][0]['snippet']['title']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count = video_response['items'][0]['statistics']['commentCount']
+            self.url = f'https://www.youtube.com/video/{self.video_id}'
+        except IndexError:
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.url = None
 
     def __str__(self):
         """
@@ -40,6 +47,7 @@ class PLVideo(Video):
     - количество лайков
     - id плейлиста
     """
+
     def __init__(self, video_id, playlist_id: str):
         super().__init__(video_id)
         self.playlist_id = playlist_id
